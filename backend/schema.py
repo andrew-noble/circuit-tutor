693e37b75ee6ac8e2f3ec57f1a12fe34d87c4e7d
Andrew Noble <aknoble.andrew@gmail.com>
2025-03-07 18:19:35 -0800

    uncomment stuff in schema.py

--- backend/schema.py
+++ backend/schema.py
@@ -24,27 +24,3 @@ class Circuit(BaseModel):
     components: list[Component]
     nets: list[Net]
     labels: list[Label]
-
-# class Pin(BaseModel):
-#     id: str
-#     net: str  # The net/wire this pin connects to
-
-# class Component(BaseModel):
-#     id: str
-#     type: str
-#     pins: list[Pin]
-#     # position: dict  # for visualization, save for later if we allow non-deterministic layouting
-
-# class Net(BaseModel):
-#     id: str
-#     name: str
-#     components: list[Component]
-
-# class Label(BaseModel):
-#     text: str
-#     target: str
-
-# class Circuit(BaseModel):
-#     components: list[Component]
-#     nets: list[Net]
-#     labels: list[Label]
