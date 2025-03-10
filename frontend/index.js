eb61c3bcdc693c15a569e4f1ae67d3148a39ccb1
Andrew Noble <aknoble.andrew@gmail.com>
2025-03-09 18:04:46 -0700

    straighten out svg positioning

--- frontend/index.js
+++ frontend/index.js
@@ -21,12 +21,12 @@ const svg = d3
 // domain is data-based (like 0-100%), range is pixel based (like 0-400px).
 // I suspect range will need to be dynamic based on responsivity
 const xScale = d3.scaleLinear().domain([0, 10]).range([0, width]);
-const yScale = d3.scaleLinear().domain([-3, 3]).range([height, 0]); //range reversed bc y dir is down in web
+const yScale = d3.scaleLinear().domain([-2, 2]).range([height, 0]); //range reversed bc y dir is down in web
 
 const gx = svg
   .append("g")
   .classed("x-axis", true)
-  .attr("transform", `translate(0, ${height})`) //translate to bottom of svg
+  .attr("transform", `translate(0, ${height / 2})`) //translate to bottom of svg
   .call(d3.axisBottom(xScale));
 
 const gy = svg
@@ -42,7 +42,7 @@ async function loadSymbols() {
   svg.append("defs").node().appendChild(defs.cloneNode(true)); //inject defs section in this document
 }
 
-function renderCircuit() {
+function renderCircuit(circuitData) {
   const layout = generateLayout(circuitData); //generate layout
 
   // Map component types to symbol IDs
@@ -60,10 +60,14 @@ function renderCircuit() {
     .data(layout)
     .join("use")
     .attr("xlink:href", (d) => typeToSymbol[d.type]) // Map component type to symbol ID
-    .attr("x", (d) => xScale(d.position.x))
-    .attr("y", (d) => yScale(d.position.y) - 50) // Center vertically
+    // .attr("x", (d) => xScale(d.position.x + 1) - 50) //the + 1 is to just translate everything away from the left margin
+    // .attr("y", (d) => yScale(d.position.y) - 50) //the -50 is to move per the 100x100 symbol's center (hardcoded yikes)
+
     .attr("width", 100)
-    .attr("height", 100);
+    .attr("height", 100)
+    .attr("transform", `translate(${d.position.x + 1}, ${d.position.y})`)
+    //add a debugging border
+    .classed("debug-border", true);
 
   //create a group for the nets (no net data though yet)
   const netGroup = svg.append("g").classed("nets", true);
