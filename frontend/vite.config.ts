c94db370609aef77a27176c0e919f4d656bccb7f
Andrew Noble <aknoble.andrew@gmail.com>
2025-03-20 11:37:44 -0700

    configure for render deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/eng-vis/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 8000,
+    host: true, // needed for docker/container environments
+  },
 });
