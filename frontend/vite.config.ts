cd06fde338729dff2d2961fb1db92e8cfded5d34
Andrew Noble <aknoble.andrew@gmail.com>
2025-03-20 12:13:43 -0700

    update deploy config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/eng-vis/",
+  base: "/",
   build: {
     outDir: "dist",
   },
